66f69116f8ad1d8abb774d0d9e63f47d334c7f2d
Alex Wang <451959831@qq.com>
2022-03-18 23:02:13 +0800

    frontend/vue2: over the select phone project

--- vue2.0/hello-world/src/App.vue
+++ vue2.0/hello-world/src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <SelectPhone />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import SelectPhone from './components/SelectPhone.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld
+    SelectPhone
   }
 }
 </script>
---
+++ vue2.0/hello-world/src/components/SelectPhone.vue
@@ -0,0 +1,78 @@
+<template>
+  <div>
+    <div class="select-wrapper">
+      <span>选择：</span>
+      <div v-for="(phone, index) in phoneList" :key="index + phone">
+        <input type="checkbox" v-model="phone.checked"/>
+        <input type="text" class="phone" v-model="phone.value"/>
+      </div>
+    </div>
+    <div class="show-wrapper">
+      <span>展示：</span>
+      <div class="show-content">
+        <div class="select-phone">
+          <span v-for="(phone, index) in selectedPhoneList" :key="index + phone">{{ phone }}</span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      phoneList: [{
+        value: 'iphone12',
+        checked: true
+      },
+      {
+        value: 'xiaomi',
+        checked: true
+      },
+      {
+        value: 'huawei',
+        checked: false
+      }]
+    }
+  },
+  computed: {
+    selectedPhoneList () {
+      return this.phoneList.filter(item => item.checked).map(item => item.value).join(' 、')
+    }
+  },
+  created () {
+    console.log(this.selectedPhoneList)
+  }
+}
+</script>
+
+<style lang="less">
+.select-wrapper,
+.show-wrapper {
+  width: 800px;
+  height: 50px;
+  margin: 10px auto;
+  text-align: start;
+  display: flex;
+
+  .phone {
+    width: 100px;
+    height: 25px;
+    line-height: 25px;
+    font-size: 18px;
+    padding: 2px 0 2px 5px;
+    border: 0;
+    border-bottom: 1px solid #000;
+    display: inline-block;
+    overflow: hidden;
+  }
+
+  .show-content {
+    width: 500px;
+    height: 25px;
+    line-height: 25px;
+    border-bottom: 1px solid #000;
+  }
+}
+</style>
