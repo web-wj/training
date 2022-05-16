b315f942c145a36ff8dc6af98a0e08be8e85325e
web-wj <451959831@qq.com>
2022-05-16 18:32:57 +0800

    tools/cli: change the cli name

--- my-app/src/views/Alex.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="alex">
-    <h1>This is an alex page</h1>
-  </div>
-</template>
--- tools/cli/lib/init.js
+++ tools/cli/lib/init.js
@@ -10,37 +10,37 @@ const spawn = async (...args) => {
   const { spawn } = require('child_process');
   return new Promise(resolve => {
     const proc = spawn(...args);
-    proc.stdout.pipe(process.stdout); 
+    proc.stdout.pipe(process.stdout);
     proc.stderr.pipe(process.stderr);
     proc.on('close', () => {
       resolve();
     })
   })
-} 
+}
 
 module.exports = async (name) => {
   // 打印欢迎页面
   clear();
-  const welcomeTip = await figlet('portal Welcome!');
+  const welcomeTip = await figlet('cli Welcome!');
   log(`${welcomeTip}\n`);
 
   // 下载模板项目
   log(`🚀 创建项目： ${name}\n`);
-  await clone('github:web-wj/portal-template', name);
+  await clone('github:web-wj/cli-template', name);
 
   // 自动安装依赖
-  log('✈ 安装依赖......\n');
-  await spawn('npm', ['install'], {cwd: `./${name}`});
-  log(`
-🔨 安装完成  
-To get Start:
-===========================
-     cd ${name}
-     npm run serve
-===========================
-  `);
+//   log('✈ 安装依赖......\n');
+//   await spawn('npm', ['install'], {cwd: `./${name}`});
+//   log(`
+// 🔨 安装完成
+// To get Start:
+// ===========================
+//      cd ${name}
+//      npm run serve
+// ===========================
+//   `);
 
   // 启动
-  open(`http:localhost:8080`);
-  await spawn('npm ', ['run', 'serve'], {cwd: `./${name}`});
+  // open(`http:localhost:8080`);
+  // await spawn('npm ', ['run', 'serve'], {cwd: `./${name}`});
 }
