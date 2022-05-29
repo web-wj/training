a8900de8379c7c6412dc1b2ae8010c22a6b4cf21
Alex Wang <451959831@qq.com>
2022-05-29 23:56:59 +0800

    ssr: init the ssr project

---
+++ SSR/base/hello-vue3/src/utils/renderToString.js
@@ -0,0 +1,13 @@
+const { createSSRApp } = require('vue')
+const { renderToString } = require('@vue/server-renderer')
+
+const app = createSSRApp({
+  data() {
+    return {
+      user: 'John Doe'
+    }
+  },
+  template: `<div>Current user is: {{ user }}</div>`
+})
+
+const appContent = await renderToString(app)
