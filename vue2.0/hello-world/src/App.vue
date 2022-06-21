54f57bc0589c7ac2cd05f3000770b81ae8543e5e
Alex Wang <451959831@qq.com>
2022-06-21 18:21:40 +0800

    vue/vuedragger: test demo

--- vue2.0/hello-world/src/App.vue
+++ vue2.0/hello-world/src/App.vue
@@ -3,7 +3,7 @@
     <div class="mt-5 container">
       <div class="row justify-content-center border py-5">
         <div class="col-5">
-          <h4 class="mb-3">容器 A</h4>
+          <h4 class="mb-3">容器 A </h4>
           <draggable class="draggable-list" :list="list1" group="my-group">
             <div class="list-item" v-for="element in list1" :key="element.name">
               {{ element.name }}
@@ -12,7 +12,7 @@
         </div>
 
         <div class="col-5">
-          <h4 class="mb-3">容器 B</h4>
+          <h4 class="mb-3">容器 B </h4>
           <draggable class="draggable-list" :list="list2" group="my-group">
             <div class="list-item" v-for="element in list2" :key="element.name">
               {{ element.name }}
