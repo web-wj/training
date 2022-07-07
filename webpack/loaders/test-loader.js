7700de019757be04eef21731b3d0515253763ba1
Alex Wang <451959831@qq.com>
2022-07-08 00:09:38 +0800

    webpack: loader test

--- webpack/loaders/test-loader.js
+++ webpack/loaders/test-loader.js
@@ -4,9 +4,8 @@ module.exports = function (sourceCode) {
   // 将 变量 a = 1 做字符串替换
   console.log('loader 运行了');
   // this 中内容太多了，使用第三方库 loader-utils 来处理
-  console.log(this);
+  const replaceSourceCode = sourceCode.replace('变量', 'const');
   const options = loaderUtils.getOptions(this)
-  console.log(options);
   const reg = new RegExp(options.changeVal, "g");
-  return sourceCode.replace(reg, 'const');
+  return replaceSourceCode.replace(reg, 'const');
 }
--- webpack/src/index.js
+++ webpack/src/index.js
@@ -1,6 +1,17 @@
-console.log('hello webpack');
-console.log('index module');
+// console.log('hello webpack');
+// console.log('index module');
 
-const a = require('./a');
-// ccc.a();
-console.log(a);
+// const a = require('./a');
+// // ccc.a();
+// console.log(a);
+
+// 处理样式
+// const content = require('./assets/style.css');
+// console.log(content); 
+
+// 处理图片
+var src = require("./assets/webpack.png")
+console.log(src);
+var img = document.createElement("img")
+img.src = src;
+document.body.appendChild(img);
